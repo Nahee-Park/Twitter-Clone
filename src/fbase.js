e321c26c5fa1d886f026a94de5e62e591fa9b503
nahee-park <jessica396@ewhain.net>
2021-08-04 06:15:56 +0900

    feat: firebase Auth 추가

--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -9,4 +10,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+// 여기서 firebase에서 불러올 것 한 번만 불러온 이후에 다른 곳에서는 얘네를 임포트해서 씀
+export const authService = firebase.auth();
