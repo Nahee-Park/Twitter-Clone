69a6f2df2ff05b690e16f7c888b9d816f4818b8f
nahee-park <jessica396@ewhain.net>
2021-08-05 17:22:44 +0900

    feat: 기본 로그인 input창 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Auth() {
-  return <div>로그인 안돼있을 때 메인페이지</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+      console.log(email);
+    } else if (name === "password") {
+      setPassword(value);
+      console.log(password);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        {/* input창에 name속성을 줌으로써 함수 하나로 운용할 수 있도록 함 */}
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
